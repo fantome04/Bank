298816af0742051706c66328a65c4f2a8cf6113f
Maria <134010066+fantome04@users.noreply.github.com>
2024-04-18 11:16:01 +0400

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -38,7 +38,7 @@ int main()
 
     std::size_t size = sizeof(Bank) + n * sizeof(BankCell);
 
-    ptr = (Bank*)mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    Bank* ptr = (Bank*)mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if(ptr == MAP_FAILED)
     {
         std::cerr << "init mmap" << std::endl;
@@ -57,7 +57,7 @@ int main()
             exit(errno);
         }
 
-        std::string res = logic(input);
+        std::string res = logic(ptr, input);
 
         if(sem_post(sem) < 0) {
             std::cerr << "post" << std::endl;
